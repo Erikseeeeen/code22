445b47323a04bd95941531955fe2a6e20b497c84
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-10-21 20:49:19 +0200

    add endpoint for fetching (short) info of all buoys

--- server/app.py
+++ server/app.py
@@ -1,10 +1,21 @@
 from flask import Flask, request, jsonify
+import os
+import json
 
 app = Flask(__name__)
 
-@app.get("/text")
-def get_countries():
-  response = jsonify({"text": "Hello world!"})
-  response.headers.add("Access-Control-Allow-Origin", "*")
-  return response
-  
+data_bouys_folder = "data/buoys/"
+data_csv_folder = "data/csv/"
+
+
+@app.get("/buoys")
+def get_all_buoys():
+    entries = os.scandir("data/buoys/")
+    output = list()
+    for entry in entries:
+        with open(data_bouys_folder + entry.name, "r") as file:
+            output.append(json.load(file))
+    print(output)
+    response = jsonify(output)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
