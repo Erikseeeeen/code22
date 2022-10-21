c91e286a70dd6d87bac0e324bd3397b4416b8260
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-10-21 21:40:54 +0200

    add endpoint for fetching csv files

--- server/app.py
+++ server/app.py
@@ -1,12 +1,13 @@
 from sqlite3 import DatabaseError
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import os
 import json
 
 app = Flask(__name__)
 
-data_bouys_folder = "data/buoys/"
-data_csv_folder = "data/csv/"
+data_buoys_folder = "./data/buoys/"
+data_csv_folder = "./data/csv/"
+data_mp4_folder = "./data/video/"
 
 
 @app.after_request
@@ -17,10 +18,10 @@ def add_response_headers(response):
 
 @app.get("/buoys")
 def get_all_buoys():
-    entries = os.scandir("data/buoys/")
+    entries = os.scandir(data_buoys_folder)
     output = list()
     for entry in entries:
-        with open(data_bouys_folder + entry.name, "r") as file:
+        with open(os.path.join(data_buoys_folder, entry.name), "r") as file:
             current_buoy = json.load(file)
         buoy_short = {
             "name": current_buoy["name"],
@@ -35,7 +36,15 @@ def get_all_buoys():
 
 @app.get("/buoy/<name>")
 def get_buoy(name):
-    with open(data_bouys_folder + name + ".json") as file:
+    with open(os.path.join(data_buoys_folder, name + ".json")) as file:
         buoy = json.load(file)
     response = jsonify(buoy)
     return response
+
+
+@app.get("/data/csv/<file>")
+def get_csv_file(file):
+    csv_path = os.path.join(data_csv_folder, file + ".csv")
+    if not os.path.isfile(csv_path):
+        return "ERROR: %s is not a file!" % file
+    return send_file(csv_path, mimetype="text/csv")
