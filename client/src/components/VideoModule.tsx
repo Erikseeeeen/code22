a7211ca5e89184f6efa19435f1a6eeed061d0a85
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2022-10-22 18:31:47 +0200

    prevent video persist

--- client/src/components/VideoModule.tsx
+++ client/src/components/VideoModule.tsx
@@ -1,35 +1,41 @@
-import { Buoy, Module, ModuleType, Plot } from '../types';
-import './ModuleContent.css';
-import Three from './three/Three';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Papa from 'papaparse';
-import { LinePlot } from './Line';
-import Loading from './Loading';
+import { Buoy, Module, ModuleType, Plot } from "../types";
+import "./ModuleContent.css";
+import Three from "./three/Three";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Papa from "papaparse";
+import { LinePlot } from "./Line";
+import Loading from "./Loading";
 
 function VideoModule({ module, buoy }: { module: Module; buoy: Buoy }) {
-
   const [video, setVideo] = useState("");
-  
-    useEffect(() => {
-        for (const sensor of buoy.sensors) {
-            if (sensor.format == "mp4") {
-                axios.get<Blob>(import.meta.env.VITE_API_URL + '/data/video/' + sensor.name, {responseType: 'blob'}).then((res) => {
-                const b = new Blob([res.data]);
-                const url = URL.createObjectURL(b);
-                setVideo(url);
-                });
-            }
-        }
-    }, [buoy.name]);
 
-    return video ? (
-        <div className="moduleContent">
-            <video width="100%" height="100%" autoPlay loop key={video}>
-            <source src={video} />
-            </video>
-        </div> 
-    ) : <Loading />;
+  useEffect(() => {
+    const video_sensor = buoy.sensors.find((s) => s.format == "mp4");
+    if (video_sensor) {
+      axios
+        .get<Blob>(
+          import.meta.env.VITE_API_URL + "/data/video/" + video_sensor.name,
+          { responseType: "blob" }
+        )
+        .then((res) => {
+          const b = new Blob([res.data]);
+          setVideo(URL.createObjectURL(b));
+        });
+    } else {
+      setVideo("");
+    }
+  }, [buoy.name]);
+
+  return video ? (
+    <div className="moduleContent">
+      <video width="100%" height="100%" autoPlay loop key={video}>
+        <source src={video} />
+      </video>
+    </div>
+  ) : (
+    <Loading />
+  );
 }
 
 export default VideoModule;
