fd50125ef04ff809b2d81537132cec0ab8eb07f2
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-10-22 10:49:08 +0200

    fix warnings in frontend

--- client/src/pages/Overview.tsx
+++ client/src/pages/Overview.tsx
@@ -1,15 +1,15 @@
-import { useContext, useEffect } from 'react';
-import Map from '../components/Map';
-import Warnings from '../components/Warnings';
-import { AppContext } from '../context';
-import axios from 'axios';
-import './Overview.css';
+import { useContext, useEffect } from "react";
+import Map from "../components/Map";
+import Warnings from "../components/Warnings";
+import { AppContext } from "../context";
+import axios from "axios";
+import "./Overview.css";
 
 function Links() {
   const context = useContext(AppContext);
 
   useEffect(() => {
-    axios.get(import.meta.env.VITE_API_URL + '/buoys').then((res) => {
+    axios.get(import.meta.env.VITE_API_URL + "/buoys").then((res) => {
       context.buoys.set(res.data);
     });
   }, []);
