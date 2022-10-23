74e4b0a815a2c1c21297cb3fc3301d0e5877c106
magnetenstad <tenstad.magne@gmail.com>
2022-10-23 11:13:30 +0200

    Edit health module css

--- client/src/components/HealthModule.tsx
+++ client/src/components/HealthModule.tsx
@@ -1,10 +1,10 @@
-import { Buoy, Module, Plot } from "../types";
-import "./ModuleContent.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Papa from "papaparse";
-import { LinePlot } from "./Line";
-import { flexbox } from "@mui/system";
+import { Buoy, Module, Sensor, Warning } from '../types';
+import './ModuleContent.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Papa from 'papaparse';
+import './HealthModule.css';
+import { formatName } from '../utils';
 
 function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
   const [lastBatteryFraction, setLastBatteryFraction] = useState<
@@ -16,12 +16,21 @@ function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
   const [lastSurfaceTime, setLastSurfaceTime] = useState<number | undefined>(
     undefined
   );
+
+  const isOk = (w: Warning) =>
+    !(
+      w.threshold.length ||
+      w.warning.length ||
+      w.rows.length ||
+      w.diffs.length
+    );
+
   useEffect(() => {
     const fetchMetaData = async () => {
       for (const sensor of buoy.sensors) {
-        if (sensor.format !== "metadata") continue;
+        if (sensor.format !== 'metadata') continue;
         await axios
-          .get(import.meta.env.VITE_API_URL + "/data/csv/" + sensor.name)
+          .get(import.meta.env.VITE_API_URL + '/data/csv/' + sensor.name)
           .then((res) => {
             const jsonData = Papa.parse(res.data, { header: true }).data;
             console.log(jsonData);
@@ -29,9 +38,9 @@ function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
               ? jsonData[jsonData.length - 1]
               : jsonData[jsonData.length - 2];
             console.log(datarow);
-            setLastBatteryFraction(datarow["last_battery_fraction"] / 100);
-            setLastSurfaceTime(+datarow["last_surface_time"]);
-            setLastServiceTime(+datarow["last_serviced_time"]);
+            setLastBatteryFraction(datarow['last_battery_fraction'] / 100);
+            setLastSurfaceTime(+datarow['last_surface_time']);
+            setLastServiceTime(+datarow['last_serviced_time']);
           });
       }
     };
@@ -39,103 +48,88 @@ function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
   }, [buoy.name]);
 
   return (
-    <div className='moduleContent'>
-      <div className='healthModule'>
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <h2 style={{ position: "absolute", left: 0, top: 0, margin: 12 }}>
+    <div className="moduleContent">
+      <div className="healthModule">
+        <div>
+          <h2 style={{ position: 'absolute', left: 0, top: 0, margin: '1em' }}>
             Health
           </h2>
-          <h3 style={{ position: "absolute", right: 0, top: 0, margin: 12 }}>
-            Last surface time:{" "}
+          <p style={{ margin: 0, marginTop: '20pt' }}>
+            Last surface time:{' '}
             {lastSurfaceTime
               ? new Date(lastSurfaceTime * 1000).toLocaleString()
-              : "No data"}
-          </h3>
+              : 'No data'}
+          </p>
+          <p style={{ margin: 0 }}>
+            Buoy last serviced:{' '}
+            {lastServiceTime
+              ? new Date(lastServiceTime * 1000).toLocaleString()
+              : 'No data'}
+          </p>
         </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <div style={{ margin: 32 }}>
-            <p>
-              Buoy last serviced:{" "}
-              {lastServiceTime
-                ? new Date(lastServiceTime * 1000).toLocaleString()
-                : "No data"}
-            </p>
-          </div>
-          <div>
-            <div style={{ fontSize: 14, margin: 32 }}>
-              {buoy.warnings.map((m) => (
-                <div>
-                  <br />
-                  <b>{m.name}</b>
-                  <ul style={{ margin: 0 }}>
-                    <li style={{ margin: 0 }}>
-                      {m.diffs.length
-                        ? "Sudden changes - See red boxes in graph"
-                        : ""}
-                    </li>
-                    <li style={{ margin: 0 }}>
-                      {m.rows.length ? "Deviating values detected" : ""}
-                    </li>
-                    <li style={{ margin: 0 }}>
-                      {m.warning.length ? "Outside warning thresholds" : ""}
-                    </li>
-                    <li style={{ margin: 0 }}>
-                      {m.threshold.length ? "Outside limit thresholds" : ""}
-                    </li>
-                    <li style={{ margin: 0 }}>
-                      {!(
-                        m.threshold.length ||
-                        m.warning.length ||
-                        m.rows.length ||
-                        m.diffs.length
-                      )
-                        ? "All ok!"
-                        : ""}
-                    </li>
-                  </ul>
-                </div>
-              ))}
-            </div>
+        <div>
+          <div style={{ fontSize: 14, margin: 0 }}>
+            {buoy.warnings.map((w) => (
+              <div>
+                <br />
+                <b style={{ color: isOk(w) ? 'var(--green)' : 'var(--red)' }}>
+                  {formatName(w.name)}
+                </b>
+                <ul>
+                  <li>
+                    {w.diffs.length
+                      ? 'Sudden changes - See red boxes in graph'
+                      : ''}
+                  </li>
+                  <li>{w.rows.length ? 'Deviating values detected' : ''}</li>
+                  <li>
+                    {w.warning.length ? 'Outside warning thresholds' : ''}
+                  </li>
+                  <li>
+                    {w.threshold.length ? 'Outside limit thresholds' : ''}
+                  </li>
+                  <li>{isOk(w) ? 'All ok!' : ''}</li>
+                </ul>
+              </div>
+            ))}
           </div>
           <div
             style={{
-              width: "100%",
+              width: '100%',
               height: 60,
-              position: "absolute",
+              position: 'absolute',
               bottom: 0,
               left: 0,
-              marginBottom: 10,
-              backgroundColor: "rgba(100, 30, 0, 0.05)",
+              marginBottom: 0,
+              backgroundColor: 'rgba(100, 30, 0, 0.05)',
             }}
           >
             <div
               style={{
-                backgroundColor: "green",
+                backgroundColor: lastBatteryFraction
+                  ? lastBatteryFraction > 0.25
+                    ? 'var(--green)'
+                    : 'var(--red)'
+                  : 'grey',
                 width: lastBatteryFraction
-                  ? 100 * lastBatteryFraction + "%"
-                  : "100%",
+                  ? 100 * lastBatteryFraction + '%'
+                  : '100%',
                 height: 60,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
               <span
                 style={{
-                  textAlign: "center",
-                  color: "white",
-                  textShadow: "0px 0px 2px black",
+                  textAlign: 'center',
+                  color: 'white',
+                  // textShadow: '0px 0px 2px black',
                 }}
               >
                 {lastBatteryFraction
-                  ? lastBatteryFraction * 100 + "%"
-                  : "No battery data"}
+                  ? lastBatteryFraction * 100 + '%'
+                  : 'No battery data'}
               </span>
             </div>
           </div>
