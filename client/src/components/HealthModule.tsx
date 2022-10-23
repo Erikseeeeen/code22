6e373349ff03736593ab6710256e5ee6b4941f93
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2022-10-23 12:38:42 +0200

    data

--- client/src/components/HealthModule.tsx
+++ client/src/components/HealthModule.tsx
@@ -1,10 +1,10 @@
-import { Buoy, Module, Sensor, Warning } from '../types';
-import './ModuleContent.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Papa from 'papaparse';
-import './HealthModule.css';
-import { formatWarningName } from '../utils';
+import { Buoy, Module, Sensor, Warning } from "../types";
+import "./ModuleContent.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Papa from "papaparse";
+import "./HealthModule.css";
+import { formatName, formatWarningName } from "../utils";
 
 function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
   const [lastBatteryFraction, setLastBatteryFraction] = useState<
@@ -28,17 +28,17 @@ function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
   useEffect(() => {
     const fetchMetaData = async () => {
       for (const sensor of buoy.sensors) {
-        if (sensor.format !== 'metadata') continue;
+        if (sensor.format !== "metadata") continue;
         await axios
-          .get(import.meta.env.VITE_API_URL + '/data/csv/' + sensor.name)
+          .get(import.meta.env.VITE_API_URL + "/data/csv/" + sensor.name)
           .then((res) => {
             const jsonData = Papa.parse(res.data, { header: true }).data;
             const datarow: any = jsonData[jsonData.length - 1]
               ? jsonData[jsonData.length - 1]
               : jsonData[jsonData.length - 2];
-            setLastBatteryFraction(datarow['last_battery_fraction'] / 100);
-            setLastSurfaceTime(+datarow['last_surface_time']);
-            setLastServiceTime(+datarow['last_serviced_time']);
+            setLastBatteryFraction(datarow["last_battery_fraction"] / 100);
+            setLastSurfaceTime(+datarow["last_surface_time"]);
+            setLastServiceTime(+datarow["last_serviced_time"]);
           });
       }
     };
@@ -46,23 +46,23 @@ function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
   }, [buoy.name]);
 
   return (
-    <div className="moduleContent">
-      <div className="healthModule">
+    <div className='moduleContent'>
+      <div className='healthModule'>
         <div>
-          <h2 style={{ position: 'absolute', left: 0, top: 0, margin: '1em' }}>
+          <h2 style={{ position: "absolute", left: 0, top: 0, margin: "1em" }}>
             Health
           </h2>
-          <p style={{ margin: 0, marginTop: '20pt' }}>
-            Last surface time:{' '}
+          <p style={{ margin: 0, marginTop: "20pt" }}>
+            Last surface time:{" "}
             {lastSurfaceTime
               ? new Date(lastSurfaceTime * 1000).toLocaleString()
-              : 'No data'}
+              : "No data"}
           </p>
           <p style={{ margin: 0 }}>
-            Buoy last serviced:{' '}
+            Buoy last serviced:{" "}
             {lastServiceTime
               ? new Date(lastServiceTime * 1000).toLocaleString()
-              : 'No data'}
+              : "No data"}
           </p>
         </div>
         <div>
@@ -70,64 +70,64 @@ function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
             {buoy.warnings.map((w) => (
               <div>
                 <br />
-                <b style={{ color: isOk(w) ? 'var(--green)' : 'var(--red)' }}>
+                <b style={{ color: isOk(w) ? "var(--green)" : "var(--red)" }}>
                   {formatWarningName(w.name, buoy)}
                 </b>
                 <ul>
                   <li>
                     {w.diffs.length
-                      ? 'Sudden changes - See red boxes in graph'
-                      : ''}
+                      ? "Sudden changes - See red boxes in graph"
+                      : ""}
                   </li>
-                  <li>{w.rows.length ? 'Deviating values detected' : ''}</li>
+                  <li>{w.rows.length ? "Deviating values detected" : ""}</li>
                   <li>
-                    {w.warning.length ? 'Outside warning thresholds' : ''}
+                    {w.warning.length ? "Outside warning thresholds" : ""}
                   </li>
                   <li>
-                    {w.threshold.length ? 'Outside limit thresholds' : ''}
+                    {w.threshold.length ? "Outside limit thresholds" : ""}
                   </li>
-                  <li>{isOk(w) ? 'All ok!' : ''}</li>
+                  <li>{isOk(w) ? "All ok!" : ""}</li>
                 </ul>
               </div>
             ))}
           </div>
           <div
             style={{
-              width: '100%',
+              width: "100%",
               height: 60,
-              position: 'absolute',
+              position: "absolute",
               bottom: 0,
               left: 0,
               marginBottom: 0,
-              backgroundColor: 'rgba(100, 30, 0, 0.05)',
+              backgroundColor: "rgba(100, 30, 0, 0.05)",
             }}
           >
             <div
               style={{
                 backgroundColor: lastBatteryFraction
                   ? lastBatteryFraction > 0.25
-                    ? 'var(--green)'
-                    : 'var(--red)'
-                  : 'grey',
+                    ? "var(--green)"
+                    : "var(--red)"
+                  : "grey",
                 width: lastBatteryFraction
-                  ? 100 * lastBatteryFraction + '%'
-                  : '100%',
+                  ? 100 * lastBatteryFraction + "%"
+                  : "100%",
                 height: 60,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
               <span
                 style={{
-                  textAlign: 'center',
-                  color: 'white',
+                  textAlign: "center",
+                  color: "white",
                   // textShadow: '0px 0px 2px black',
                 }}
               >
                 {lastBatteryFraction
-                  ? lastBatteryFraction * 100 + '%'
-                  : 'No battery data'}
+                  ? lastBatteryFraction * 100 + "%"
+                  : "No battery data"}
               </span>
             </div>
           </div>
--- client/src/components/MerdeModule.tsx
+++ client/src/components/MerdeModule.tsx
@@ -1,36 +1,30 @@
-import { Buoy, Module, Sensor, Warning } from '../types';
-import './ModuleContent.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Papa from 'papaparse';
-import './MerdeModule.css';
-import { formatName } from '../utils';
+import { Buoy, Module, Sensor, Warning } from "../types";
+import "./ModuleContent.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Papa from "papaparse";
+import "./MerdeModule.css";
+import { formatName } from "../utils";
 
 function MerdeModule({ module, buoy }: { module: Module; buoy: Buoy }) {
-  const [pH, setPH] = useState<
-    number | undefined
-  >(undefined);
-  const [liceStatus, setLiceStatus] = useState<number | undefined>(
-    undefined
-  );
-  const [fishMass, setFishMass] = useState<number | undefined>(
-    undefined
-  );
+  const [pH, setPH] = useState<number | undefined>(undefined);
+  const [liceStatus, setLiceStatus] = useState<number | undefined>(undefined);
+  const [fishMass, setFishMass] = useState<number | undefined>(undefined);
 
   useEffect(() => {
     const fetchMerdeData = async () => {
       for (const sensor of buoy.sensors) {
-        if (sensor.format !== 'merde') continue;
+        if (sensor.format !== "merde") continue;
         await axios
-          .get(import.meta.env.VITE_API_URL + '/data/csv/' + sensor.name)
+          .get(import.meta.env.VITE_API_URL + "/data/csv/" + sensor.name)
           .then((res) => {
             const jsonData = Papa.parse(res.data, { header: true }).data;
             const datarow: any = jsonData[jsonData.length - 1]
               ? jsonData[jsonData.length - 1]
               : jsonData[jsonData.length - 2];
-            setPH(datarow['pH']);
-            setFishMass(datarow['fish_mass']);
-            setLiceStatus(datarow['lice_status']);
+            setPH(datarow["pH"]);
+            setFishMass(datarow["fish_mass"]);
+            setLiceStatus(datarow["lice_status"]);
           });
       }
     };
@@ -38,22 +32,18 @@ function MerdeModule({ module, buoy }: { module: Module; buoy: Buoy }) {
   }, [buoy.name]);
 
   return (
-    <div className="moduleContent">
-      <div className="merdeModule">
+    <div className='moduleContent'>
+      <div className='merdeModule'>
         <div>
-          <h2 style={{ position: 'absolute', left: 0, top: 0, margin: '1em' }}>
+          <h2 style={{ position: "absolute", left: 0, top: 0, margin: "1em" }}>
             Fish status
           </h2>
-          <p style={{ margin: 0, marginTop: '20pt' }}>
+          <p style={{ margin: 0, marginTop: "20pt" }}>
             Average fish mass: {fishMass}kg
           </p>
-          <p style={{ margin: 0 }}>
-            Current lice prevelance: {liceStatus}
-            
-          </p>
-          <p style={{ margin: 0 }}>
-            pH value: {pH}
-          </p>
+          <p style={{ margin: 0 }}>Current lice prevelance: {liceStatus}</p>
+          <p style={{ margin: 0 }}>pH value: {pH}</p>
+          <img src='/salmon.png' width='100%' style={{ marginTop: 40 }} />
         </div>
       </div>
     </div>
