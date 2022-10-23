635e7303ba784a16019c26a7f797b31c458ccfc5
Erikseeeeen <eivindseriksen@gmail.com>
2022-10-23 13:04:45 +0200

    draft

--- draft/main.py
+++ draft/main.py
@@ -3,8 +3,13 @@ from socket import AF_INET, SOCK_STREAM
 s = socket.socket(AF_INET, SOCK_STREAM)
 s.connect(("192.168.1.177", 80))
 s.send(b"GET / HTTP/1.1\r\n")
-while True:
+read = True
+while read:
     data = s.recv(4096*8).decode("utf-8")
     for i in data.split("\r\n"):
         if i:
+            data+=i
             print(i)
+            if(i=="DONE"):
+                read = False
+print(data)
