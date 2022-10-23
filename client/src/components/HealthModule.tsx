96ddb4e2d3a2bcd306bb704f193bba5c9ded266d
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2022-10-23 10:42:57 +0200

    Health

--- client/src/components/HealthModule.tsx
+++ client/src/components/HealthModule.tsx
@@ -4,11 +4,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Papa from "papaparse";
 import { LinePlot } from "./Line";
+import { flexbox } from "@mui/system";
 
 function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
-  const [lastBatteryFraction, setLastBatteryFraction] = useState(1.0);
-  const [lastServiceTime, setLastServiceTime] = useState(0.0);
-  const [lastSurfaceTime, setLastSurfaceTime] = useState(0.0);
+  const [lastBatteryFraction, setLastBatteryFraction] = useState<
+    number | undefined
+  >(undefined);
+  const [lastServiceTime, setLastServiceTime] = useState<number | undefined>(
+    undefined
+  );
+  const [lastSurfaceTime, setLastSurfaceTime] = useState<number | undefined>(
+    undefined
+  );
   useEffect(() => {
     const fetchMetaData = async () => {
       for (const sensor of buoy.sensors) {
@@ -17,16 +24,12 @@ function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
           .get(import.meta.env.VITE_API_URL + "/data/csv/" + sensor.name)
           .then((res) => {
             const jsonData = Papa.parse(res.data, { header: true }).data;
-
-            jsonData.forEach((datarow: any) => {
-              const lastBatteryFraction: number =
-                datarow["last_battery_fraction"];
-              setLastBatteryFraction(lastBatteryFraction);
-              const lastSurfaceTime: number = datarow["last_surface_time"];
-              setLastServiceTime(lastSurfaceTime);
-              const lastServiceTime: number = datarow["last_service_time"];
-              setLastSurfaceTime(lastServiceTime);
-            });
+            console.log(jsonData);
+            const datarow: any = jsonData[jsonData.length - 2];
+            console.log(datarow);
+            setLastBatteryFraction(datarow["last_battery_fraction"] / 100);
+            setLastSurfaceTime(+datarow["last_surface_time"]);
+            setLastServiceTime(+datarow["last_serviced_time"]);
           });
       }
     };
@@ -34,14 +37,108 @@ function HealthModule({ module, buoy }: { module: Module; buoy: Buoy }) {
   }, [buoy.name]);
 
   return (
-    <div className="moduleContent">
-      <p>
-        Last surface time: {new Date(lastSurfaceTime * 1000).toLocaleString()}
-        <br />
-        Buoy last serviced: {new Date(lastServiceTime * 1000).toLocaleString()}
-        <br />
-        Last known battery percentage: {lastBatteryFraction * 100}%<br />
-      </p>
+    <div className='moduleContent'>
+      <div className='healthModule'>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <h2 style={{ position: "absolute", left: 0, top: 0, margin: 12 }}>
+            Health
+          </h2>
+          <h3 style={{ position: "absolute", right: 0, top: 0, margin: 12 }}>
+            Last surface time:{" "}
+            {lastSurfaceTime
+              ? new Date(lastSurfaceTime * 1000).toLocaleString()
+              : "No data"}
+          </h3>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ margin: 32 }}>
+            <p>
+              Buoy last serviced:{" "}
+              {lastServiceTime
+                ? new Date(lastServiceTime * 1000).toLocaleString()
+                : "No data"}
+            </p>
+          </div>
+          <div>
+            <div style={{ fontSize: 14, margin: 32 }}>
+              {buoy.warnings.map((m) => (
+                <div>
+                  <br />
+                  <b>{m.name}</b>
+                  <ul style={{ margin: 0 }}>
+                    <li style={{ margin: 0 }}>
+                      {m.diffs.length
+                        ? "Sudden changes - See red boxes in graph"
+                        : ""}
+                    </li>
+                    <li style={{ margin: 0 }}>
+                      {m.rows.length ? "Deviating values detected" : ""}
+                    </li>
+                    <li style={{ margin: 0 }}>
+                      {m.warning.length ? "Outside warning thresholds" : ""}
+                    </li>
+                    <li style={{ margin: 0 }}>
+                      {m.threshold.length ? "Outside limit thresholds" : ""}
+                    </li>
+                    <li style={{ margin: 0 }}>
+                      {!(
+                        m.threshold.length ||
+                        m.warning.length ||
+                        m.rows.length ||
+                        m.diffs.length
+                      )
+                        ? "All ok!"
+                        : ""}
+                    </li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div
+            style={{
+              width: "100%",
+              height: 60,
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              marginBottom: 10,
+              backgroundColor: "rgba(100, 30, 0, 0.05)",
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: "green",
+                width: lastBatteryFraction
+                  ? 100 * lastBatteryFraction + "%"
+                  : "100%",
+                height: 60,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <span
+                style={{
+                  textAlign: "center",
+                  color: "white",
+                  textShadow: "0px 0px 2px black",
+                }}
+              >
+                {lastBatteryFraction
+                  ? lastBatteryFraction * 100 + "%"
+                  : "No battery data"}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
