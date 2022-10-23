22d31140c7a0ca156a08c66f006e61d924360006
magnetenstad <tenstad.magne@gmail.com>
2022-10-23 10:47:55 +0200

    Edit css

--- client/src/pages/Overview.tsx
+++ client/src/pages/Overview.tsx
@@ -8,6 +8,7 @@ import './Overview.css';
 import Loading from '../components/Loading';
 import CustomPopup from '../components/CustomPopup';
 import BuoyConfig from '../components/BuoyConfig';
+import { FaPlus } from 'react-icons/fa';
 
 function Overview() {
   const context = useContext(AppContext);
@@ -47,23 +48,8 @@ function Overview() {
       </div>
       <CustomPopup
         trigger={
-          <button className="add-buoy">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="50"
-                height="50"
-                fill="currentColor"
-                className="bi bi-plus-lg"
-                viewBox="0 0 16 16"
-                display="block"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-                />
-              </svg>
-            </div>
+          <button className="btn-outline">
+            <FaPlus /> Buoy
           </button>
         }
       >
