3bf3fd31c6ebd84b5a8fed9575579937fa06cb05
GisleWink <stian.alseth@hotmail.no>
2022-10-24 16:00:41 +0200

    added oppan blender style flight controller to sonarmodule

--- client/src/components/SonarModule.tsx
+++ client/src/components/SonarModule.tsx
@@ -7,6 +7,8 @@ import { LinePlot } from "./Line";
 import * as THREE from "three";
 import { OBJLoader } from "three-stdlib";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FlyControls } from "three-stdlib";
+import { PointerLockControls } from "three-stdlib";
 import { Euler } from "three";
 
 function SonarModule({ module, buoy }: { module: Module; buoy: Buoy }) {
@@ -42,19 +44,25 @@ function SonarModule({ module, buoy }: { module: Module; buoy: Buoy }) {
           element.current?.clientHeight ?? 100
         );
         //renderer.setSize(400, 400);
-        // renderer.render(scene, camera);
-        // console.log("dean")
-        var spinnnin=2
+        // camera.position.x = 6;
+        // camera.position.y = 3.5;
+        // camera.position.z = -2;
+        // camera.position. = { x: 2.9514219402882294, y: 6.890118963801542, z: 2.680170703679929 }
+        camera.position.x = 7.103141816153798
+        camera.position.y = 4.714709232051763
+        camera.position.z = -4.350312930872963
+        const flycontroller = new FlyControls( camera, renderer.domElement);
+        flycontroller.movementSpeed = 0.005
+        flycontroller.dragToLook = true
+        flycontroller.rollSpeed = 0.0025
         function animate() {
           requestAnimationFrame( animate );
-          camera.position.x = 6;
-          camera.position.y = 3.5;
-          camera.position.z = -2;
-          spinnnin+=0.1
-          camera.setRotationFromEuler(new Euler(0, spinnnin, 0));
+          flycontroller.update(1000/100);
           renderer.render( scene, camera );
+          // console.log(camera.position);
         }
         animate();
+        
 
         if (element.current?.firstElementChild != null)
           element.current?.removeChild(element.current?.firstElementChild);
